469848be683f8d4c7ca84c26d392db15100642a3
Luiz Alonso <luiz.cararetoalonso@gmail.com>
2024-04-15 16:58:33 -0300

    Modulo adiciona delivery_zone_id no pivot para consulta fase inicial

---
+++ partner_delivery_zone_product_sold_report/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2018 Tecnativa - Sergio Teruel
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Delivery Zone Products Sold Report",
+    "summary": "Report products sold by delivery zone",
+    "version": "14.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Delivery",
+    "website": "https://github.com/tijwatanabe/delivery-carrier.git",
+    "author": "Jwatanabe Alimentos Ltda.",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["partner_delivery_zone"],
+    "data": [
+    ],
+}
